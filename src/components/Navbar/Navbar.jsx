dd5bd57f79853374d5ad0aae1a2ed1caa6307231
chadhas282-afk <chadhas282@gmail.com>
2026-03-07 21:05:28 +0530

    Add Navbar component: implement structure with logo and navigation links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <img src={logo} alt="CryptoCurrent Logo" />  
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className='nav-right'>
+
+      </div>
     </div>
   );
 }
