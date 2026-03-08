128dba4524ad754374b4dc4b73284c8d55b2ed6b
chadhas282-afk <chadhas282@gmail.com>
2026-03-08 10:56:11 +0530

    Add structure and styling to Home component with hero section and search form

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,15 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='home'>
+      <div className="hero">
+        <h1>Largest <br/> Cryptocurrency Marketplace</h1>
+        <p>Welcome to the world's largest Cryptocurrency Marketplace. Sign up to explore more about thousands of cryptocurrencies.</p>
+        <form>
+          <input type="text" placeholder='Search crypto' />
+          <button type='submit'>Search</button>
+        </form>
+      </div>
     </div>
   );
 }
