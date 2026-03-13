f0fd7a8dc97017fd1dda1efbd319721da4fb0146
chadhas282-afk <chadhas282@gmail.com>
2026-03-13 19:25:26 +0530

    Refactor Navbar and CoinContext to integrate currency selection and state management

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import arrow_icon from "../../assets/arrow_icon.png";
+import { CoinContext } from '../../context/CoinContext';
 
 const Navbar = () => {
+
+  const {setCurrency} = useContext(CoinContext);
+  const currencyHandler = (e) => {
+    switch (e.target.value) {
+      case "usd":
+        setCurrency({ name: "USD", symbol: "$" });
+        break;
+      case "eur":
+        setCurrency({ name: "EUR", symbol: "€" });
+        break;
+      case "inr":
+        setCurrency({ name: "INR", symbol: "₹" });
+        break;
+      default:
+        setCurrency({ name: "USD", symbol: "$" });
+    }
+  }
   return (
     <div className='navbar'>
       <img className='logo' src={logo} alt="CryptoCurrent Logo" />  
@@ -14,7 +32,7 @@ const Navbar = () => {
         <li>Blog</li>
       </ul>
       <div className='nav-right'>
-        <select>
+        <select onChange={(e) => setCurrency(e.target.value)}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
           <option value="inr">INR</option>
--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState} from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 
 
@@ -7,14 +7,33 @@ const CoinContext = createContext();
 const CoinContextProvider = (props) => {
 
     const [allCoins, setAllCoins] = useState([]);
+    const [currency, setCurrency] = useState({
+        name: "USD",
+        symbol: "$"
+    });
 
-    const contextValue = {}
+    const fetchAllCoin = async () => {
+        const options = { method: 'GET', headers: { 'x-cg-demo-api-key': 'CG-iB3ndzqyX6iMKjmPvHL6ZvQx' } };
 
-  return (
-    <CoinContext.Provider value={{ contextValue }}>
-      {props.children}
-    </CoinContext.Provider>
-  );
+        fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name.toLowerCase()}`, options)
+            .then(res => res.json())
+            .then(res => setAllCoins(Response))
+            .catch(err => console.error(err));
+    }
+    useEffect(() => {
+        fetchAllCoin();
+    }, [currency])
+    const contextValue = {
+        allCoins,
+        currency,
+        setCurrency
+    }
+
+    return (
+        <CoinContext.Provider value={{ contextValue }}>
+            {props.children}
+        </CoinContext.Provider>
+    );
 };
 
 export default CoinContextProvider;
