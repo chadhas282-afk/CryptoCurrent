7b76fe9791a481a47c6215ad15e4a1ac49a69d5a
chadhas282-afk <chadhas282@gmail.com>
2026-03-14 19:50:51 +0530

    Display coin ID in Coin component; update heading to show selected coin

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./Coin.css";
+import { useParams } from 'react-router-dom';
 const Coin = () => {
+  const { coinid } = useParams();
   return (
     <div>
-      <h2>Coin</h2>
+      <h2>Coin : {coinid}</h2>
     </div>
   );
 }
