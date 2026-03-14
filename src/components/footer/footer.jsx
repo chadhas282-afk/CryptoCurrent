283b125093078f784d48aad83dce8b4330502486
chadhas282-afk <chadhas282@gmail.com>
2026-03-14 12:10:39 +0530

    Add Footer component to App; include copyright notice and styling

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+
+const footer = () => {
+  return (
+    <div className='footer'>
+      <p>© 2023 CryptoCurrent. All rights reserved.</p>
+    </div>
+  );
+}
+
+export default footer;
+
