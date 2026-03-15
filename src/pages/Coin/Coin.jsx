3c91946030b82eb57f1c5989eeb5b1da35b3b576
chadhas282-afk <chadhas282@gmail.com>
2026-03-15 16:00:14 +0530

    Add loading spinner and improve coin data fetching logic in Coin component

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -2,38 +2,44 @@ import React, { useContext, useEffect, useState } from 'react';
 import "./Coin.css";
 import { useParams } from 'react-router-dom';
 import { CoinContext } from '../../context/coinContext';
+
 const Coin = () => {
   const { coinid } = useParams();
-  const [coindata, SetCoinData] = useState({});
+  const [coindata, setCoinData] = useState(); 
   const { currency } = useContext(CoinContext);
+
   const fetchCoinData = async () => {
-    const options = { method: 'GET', headers: { 'x-cg-demo-api-key': 'CG-iB3ndzqyX6iMKjmPvHL6ZvQx' } };
+    const options = { 
+      method: 'GET', 
+      headers: { 'x-cg-demo-api-key': 'CG-iB3ndzqyX6iMKjmPvHL6ZvQx' } 
+    };
 
     fetch(`https://api.coingecko.com/api/v3/coins/${coinid}`, options)
       .then(res => res.json())
-      .then(res => SetCoinData(res))
+      .then(res => setCoinData(res))
       .catch(err => console.error(err));
   }
+
   useEffect(() => {
     fetchCoinData();
   }, [currency]);
-  if (coindata) {
+
+  if (coindata && coindata.image) {
     return (
       <div className='coin'>
         <div className="coin-name">
-          <img src={coindata.img.large} alt="" />
+          <img src={coindata.image.large} alt={coindata.name} />
           <p><br /> {coindata.name} ({coindata.symbol.toUpperCase()})<br /></p>
         </div>
       </div>
     );
-  }else{
-    <div className="spinner">
-      <div className="spin">
-        
+  } else {
+    return (
+      <div className="spinner">
+        <div className="spin"></div>
       </div>
-    </div>
+    );
   }
-
 }
 
 export default Coin;
