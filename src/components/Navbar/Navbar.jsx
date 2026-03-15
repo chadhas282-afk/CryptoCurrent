0b8c7fe5e4f6283af095cd03d946e4de9f02e3df
chadhas282-afk <chadhas282@gmail.com>
2026-03-15 17:40:41 +0530

    Remove unused arrow icon and clean up Navbar component; refactor imports and remove unnecessary elements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import React, { useContext } from 'react';
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
-import arrow_icon from "../../assets/arrow_icon.png";
-import { CoinContext } from '../../context/CoinContext';
+import { CoinContext } from '../../context/coinContext';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -28,21 +27,12 @@ const Navbar = () => {
       <Link to="/">
       <img className='logo' src={logo} alt="CryptoCurrent Logo" />  
       </Link>
-      <ul>
-        <Link to="/">
-        <li>Home</li>
-        </Link>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>Blog</li>
-      </ul>
       <div className='nav-right'>
         <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
           <option value="inr">INR</option>
         </select>
-        <button>Sign Up <img src={arrow_icon} alt="" /></button>
       </div>
     </div>
   );
